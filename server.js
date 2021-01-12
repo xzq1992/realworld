6f8e1c122f2175e29c8ad6238c9fefa87e4a27b2
xzq1992 <1194860219@qq.com>
2021-01-12 23:44:40 +0800

    push

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const server = http.createServer((req,res) => {
-  res.setHeader('Content-Type','text/html;charset=utf-8')
-  res.end('拉勾教育')
-})
-server.listen(4000,()=>{
-  console.log('服务器运行了')
-})
